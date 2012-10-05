9e67ecf2d51f4c2008df350602d8cf47fcc07012
Trevor Gerhardt <trevorgerhardt@gmail.com>
2012-10-04 20:39:03 -0400

    Update view

--- index.js
+++ index.js
@@ -7,53 +7,55 @@ var Backbone = require('backbone')
   , _ = require('lodash');
 
 /**
- * Export `View`
+ * Export extended view
  */
 
-var View = module.exports = Backbone.View.extend();
+module.exports = Backbone.View.extend({
 
-/**
- * Setup, called on initialize
- */
+  /**
+   * Setup, called on initialize
+   */
 
-View.prototype.setup = function() {
-  this.views = {};
-  this.rendered = false;
-  this.on('renderedStateChange', function(rendered) {
-    this.rendered = rendered;
-    this.onRenderedStateChange(rendered);
-    
-    _.each(this.views, function(view, key) {
-      view.trigger('renderedStateChange', rendered);
+  setup: function() {
+    this.views = {};
+    this.rendered = false;
+    this.on('renderedStateChange', function(rendered) {
+      this.rendered = rendered;
+      this.onRenderedStateChange(rendered);
+      
+      _.each(this.views, function(view, key) {
+        view.trigger('renderedStateChange', rendered);
+      });
+
+      if (rendered) {
+        this.delegateEvents();
+      } else {
+        this.remove();
+      }
+    }, this);
+  }
+
+  /**
+   * Empty rendered state change function
+   */
+
+, onRenderedStateChange: function(rendered) {
+    return this;
+  }
+
+  /**
+   * Destroy a view
+   */
+
+, destroy: function() {
+    this.trigger('renderedStateChange', false);
+
+    if (this.collection) this.collection.off(null, null, this);
+    if (this.model) this.model.off(null, null, this);
+
+    _.each(this.views, function(view) {
+      view.destroy();
     });
+  }
 
-    if (rendered) {
-      this.delegateEvents();
-    } else {
-      this.remove();
-    }
-  }, this);
-};
-
-/**
- * Empty rendered state change function
- */
-
-View.prototype.onRenderedStateChange = function(rendered) {
-  return this;
-};
-
-/**
- * Destroy a view
- */
-
-View.prototype.destroy = function() {
-  this.trigger('renderedStateChange', false);
-
-  if (this.collection) this.collection.off(null, null, this);
-  if (this.model) this.model.off(null, null, this);
-
-  _.each(this.views, function(view) {
-    view.destroy();
-  });
-};
+});
