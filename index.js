0f27869ebd47a8a21bf325bd893ef9dafde4c003
Trevor Gerhardt <trevorgerhardt@gmail.com>
2012-10-24 17:01:20 -0400

    Fixed

--- index.js
+++ index.js
@@ -4,58 +4,56 @@
  */
 
 var Backbone = require('backbone')
-  , _ = require('lodash');
+  , _ = require('underscore');
 
 /**
  * Export extended view
  */
 
-module.exports = Backbone.View.extend({
+var View = module.exports = Backbone.View.extend();
 
-  /**
-   * Setup, called on initialize
-   */
+/**
+ * Setup, called on initialize
+ */
 
-  setup: function() {
-    this.views = {};
-    this.rendered = false;
-    this.on('renderedStateChange', function(rendered) {
-      this.rendered = rendered;
-      this.onRenderedStateChange(rendered);
-      
-      _.each(this.views, function(view, key) {
-        view.trigger('renderedStateChange', rendered);
-      });
-
-      if (rendered) {
-        this.delegateEvents();
-      } else {
-        this.remove();
-      }
-    }, this);
-  }
-
-  /**
-   * Empty rendered state change function
-   */
-
-, onRenderedStateChange: function(rendered) {
-    return this;
-  }
-
-  /**
-   * Destroy a view
-   */
-
-, destroy: function() {
-    this.trigger('renderedStateChange', false);
-
-    if (this.collection) this.collection.off(null, null, this);
-    if (this.model) this.model.off(null, null, this);
-
-    _.each(this.views, function(view) {
-      view.destroy();
+View.prototype.setup = function() {
+  this.views = {};
+  this.rendered = false;
+  this.on('renderedStateChange', function(rendered) {
+    this.rendered = rendered;
+    this.onRenderedStateChange(rendered);
+    
+    _.each(this.views, function(view, key) {
+      view.trigger('renderedStateChange', rendered);
     });
-  }
 
-});
+    if (rendered) {
+      this.delegateEvents();
+    } else {
+      this.remove();
+    }
+  }, this);
+};
+
+/**
+ * Empty rendered state change function
+ */
+
+View.prototype.onRenderedStateChange = function(rendered) {
+  return this;
+};
+
+/**
+ * Destroy a view
+ */
+
+View.prototype.destroy = function() {
+  this.trigger('renderedStateChange', false);
+
+  if (this.collection) this.collection.off(null, null, this);
+  if (this.model) this.model.off(null, null, this);
+
+  _.each(this.views, function(view) {
+    view.destroy();
+  });
+};
